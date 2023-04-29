fd42a6245d3ad376bfc5fd218e65561369a24230
Roba <127531030+FakeRoba@users.noreply.github.com>
2023-04-29 11:25:41 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+game 'gta5'
+fx_version 'cerulean'
+
+author 'Roba'
+
+client_scripts {
+    'Config/Config.lua',
+    'Client/*.lua'
+}
+
+server_scripts {
+    'Config/S_Config.lua',
+    'Server/*.lua'
+}
